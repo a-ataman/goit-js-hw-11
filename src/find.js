8b25646c536af982e210ea6054e4ebf6c4d187cc
Alice Atamanenko <alice_ataman@icloud.com>
2023-01-28 10:26:52 +0300

    commmit

---
+++ src/find.js
@@ -0,0 +1,90 @@
+import './css/style.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { onLoad } from './api-pixabay';
+import { message } from './massage';
+import { scroll } from './scroll';
+
+const inputForm = document.querySelector('form');
+const gallery = document.querySelector('.gallery');
+export const btnMore = document.querySelector('.load-more');
+btnMore.addEventListener('click', addButton);
+inputForm.addEventListener('submit', findBase);
+
+let qwest = '';
+let page = 1;
+
+async function findBase(e) {
+  e.preventDefault();
+  gallery.innerHTML = '';
+  page = 1;
+  showImg(e.target.searchQuery.value, page);
+  btnMore.classList.add("is-hidden")
+}
+
+async function showImg(qwery, page) {
+  qwest = qwery;
+  try {
+    const data = await onLoad(qwery, page);
+    renderImages(data);
+    if (page > 1) {
+      scroll();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  galleryList.refresh();
+}
+
+function renderImages(data) {
+  const { totalHits, hits } = data.data;
+  message(page, hits, totalHits);
+
+  const markup = data.data.hits
+    .map(image => {
+      const {
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = image;
+
+      return `<div class="photo-card">
+  <a href="${largeImageURL}" class="gallery__item"><img onclick="return false"; src="${webformatURL}" alt="${tags}" class="gallery__image"  loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      ${downloads}
+    </p>
+  </div>
+</div></a>`;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+function addButton() {
+  page += 1;
+
+  showImg(qwest, page);
+}
+
+let galleryList = new SimpleLightbox('div.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 500,
+});
