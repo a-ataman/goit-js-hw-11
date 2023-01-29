7ec7de10fc01737a2aaa59c02455beb0ef299a44
Alice Atamanenko <alice_ataman@icloud.com>
2023-01-29 18:22:49 +0300

    commit

--- src/server_request.js
+++ src/server_request.js
@@ -3,9 +3,8 @@ const KEY = `33212863-d1b6547963e87b60c0036ca60`;
 const MAIN_URL = `https://pixabay.com/api/`;
 
 export async function serverRequest(customRequest, page) {
-    const fulUrl = `${MAIN_URL}?key=${KEY}&q=${customRequest}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
-    const responce = await axios.get(fulUrl)
-    const data = responce.data
+    const responce = await axios.get(
+        `${MAIN_URL}?key=${KEY}&q=${customRequest}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
     
-    return data
+    return responce.data;
 }
